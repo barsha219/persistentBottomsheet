63c7b9750826373e54198a3b1c983475d863d28b
barshathapa <tbarsha128@gmail.com>
2021-10-02 13:01:32 +0545

    BottomsheetModal-barshathapa

---
+++ app/src/main/java/com/example/bottomsheet/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.bottomsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button showbtn;
+    private BottomSheetBehavior bottomSheetBehavior;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        showbtn = findViewById(R.id.showbtn);
+
+
+
+        LinearLayout bottomsheetlayout = findViewById(R.id.bottomsheetl);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomsheetlayout);
+
+        bottomSheetBehavior.setPeekHeight(50);
+
+        bottomSheetBehavior.setHideable(false);
+
+        showbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+
+
+            }
+        });
+
+
+    }
+}
